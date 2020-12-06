4952d91cb3254e0874d00917aa2d8ca995ab4f0b
khaledghaffarii <med.khaledghaffary@gmail.com>
2020-12-06 15:00:47 +0100

    style: formate nav dashboard

--- src/app/MatxLayout/SharedCompoents/Brand.jsx
+++ src/app/MatxLayout/SharedCompoents/Brand.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import {Link} from "react-router-dom";
 const Brand = ({ children }) => {
   return (
-    <div className="flex items-center justify-between brand-area">
+    <div className="flex items-center justify-between brand-area ">
       <Link to="/dashboard/analytics">
-      <div className="flex items-center brand"> 
-        <img src="/assets/images/eppm.png" alt="company-logo" />
-        <span className="brand__text">EPPM</span>
-      </div>
+      {/* <div className="flex items-center brand ml-3" style={{ maxWidth:'100%'}}> 
+        <img src="/assets/images/eppm.jpeg" alt="company-logo" />
+      </div> */}
+      <b className="ml-3" style={{ 'fontWeight':'bold','fontSize':'3em','color':'#ea0022' }}>EPPM</b>
       </Link>
       {children}
     </div>
