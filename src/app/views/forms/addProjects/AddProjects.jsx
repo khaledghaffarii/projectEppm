5f31c6dc49e77beae91c7d2b78204ad568eb94d0
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-01-06 15:08:46 +0100

    refactor: try to fusion input.js component in addProject component

--- src/app/views/forms/addProjects/AddProjects.jsx
+++ src/app/views/forms/addProjects/AddProjects.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import LoaderBounce from '../../../../matx/components/LoaderBounce';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container,Form,Button,Spinner} from 'react-bootstrap'
+import { Container,Form,Button,Spinner,InputGroup,Col,FormControl} from 'react-bootstrap'
 import Input from '../formAdd/input/input';
 import { SimpleCard} from "matx";
+//import { Form,Col,InputGroup,FormControl} from 'react-bootstrap'
+//import classes from './input.scss';
+import TextAreaComponent from '..//formAdd/TextAreaComponent'
+import ContryRegionComponent from '../formAdd/ContryRegionComponent';
 //import $ from 'jquery';²
 import axios from 'axios';
 import 'sweetalert2/src/sweetalert2.scss'
@@ -11,6 +15,8 @@ import {connect} from 'react-redux'
 //import Swal from 'sweetalert2/dist/sweetalert2.js'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+
 export class AddProjects extends Component {
    
     constructor(props){
@@ -19,74 +25,74 @@ export class AddProjects extends Component {
         this.state = {
             projectForm: {
                 name:{
-                    elementType: 'inputNomProjet',
-                    elementConfig: {
-                        type:"text" ,
-                        placeholder:"Nom du projet"
-                    },
+                    // elementType: 'inputNomProjet',
+                    // elementConfig: {
+                    //     type:"text" ,
+                    //     placeholder:"Nom du projet"
+                    // },
                     value: '',
-                    validation: {
-                        required: true
-                      },
-                      valid: false
+                    // validation: {
+                    //     required: true
+                    //   },
+                    //   valid: false
                  },
 
             categorie: {
                     
-                    elementType: 'SelectCategorieProjet',
-                    elementConfig: {
-                        type: 'text',
-                        options: 
-                        [
-                            {value: 'oilgaz', displayValue: 'Gaz et Pétrole'},
-                            {value: 'water', displayValue: "Traitement de l'eau"},
-                            {value: 'energy', displayValue: 'Energie renouvelable'},
-                            {value: 'operation', displayValue: 'Operation et Maintenance'},
-                            {value: 'industry', displayValue: 'Industrie'},
-                        ]
+                    // elementType: 'SelectCategorieProjet',
+                    // elementConfig: {
+                    //     type: 'text',
+                    //     options: 
+                    //     [
+                    //         {value: 'oilgaz', displayValue: 'Gaz et Pétrole'},
+                    //         {value: 'water', displayValue: "Traitement de l'eau"},
+                    //         {value: 'energy', displayValue: 'Energie renouvelable'},
+                    //         {value: 'operation', displayValue: 'Operation et Maintenance'},
+                    //         {value: 'industry', displayValue: 'Industrie'},
+                    //     ]
                         
-                    },
+                    // },
                     value: '',
-                    validation: {},
-                      valid: false
+                    // validation: {},
+                    //   valid: false
             },
             location: {
-                    elementType: 'selectLocation',
-                    elementConfig: {
-                        type: 'text',
+                    // elementType: 'selectLocation',
+                    // elementConfig: {
+                    //     type: 'text',
                         
-                    },
+                    // },
                     value: '',
-                    validation: {
-                        required: true,
-                        valid: false,
-                        touched: false, 
-                    },
-                      valid: false,
-                      touched: false
-                },
+                //     validation: {
+                //         required: true,
+                //         valid: false,
+                //         touched: false, 
+                //     },
+                //       valid: false,
+                //       touched: false
+                 },
             customer: {
-                    elementType: 'selectClient',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'Client',
-                        options:
-                        [ //'MSBI', 'STAROIL', 'ECM+', 'BRITSH GAS TUNISIA'
-                         //,'STAR CONTRACTING'
-                            {value: 'MSBI', displayValue: 'MSBI'},
-                            {value: 'STAROIL', displayValue: 'STAROIL'},
-                            {value: 'ECM+', displayValue: 'ECM+'},
-                            {value: 'BRITSH GAS TUNISIA', displayValue: 'BRITSH GAS TUNISIA'},
-                            {value: 'STAR CONTRACTING', displayValue: 'STAR CONTRACTING'},
-                        ]
+                    // elementType: 'selectClient',
+                    // elementConfig: {
+                    //     type: 'text',
+                    //     placeholder: 'Client',
+                    //     options:
+                    //     [ //'MSBI', 'STAROIL', 'ECM+', 'BRITSH GAS TUNISIA'
+                    //      //,'STAR CONTRACTING'
+                    //         {value: 'MSBI', displayValue: 'MSBI'},
+                    //         {value: 'STAROIL', displayValue: 'STAROIL'},
+                    //         {value: 'ECM+', displayValue: 'ECM+'},
+                    //         {value: 'BRITSH GAS TUNISIA', displayValue: 'BRITSH GAS TUNISIA'},
+                    //         {value: 'STAR CONTRACTING', displayValue: 'STAR CONTRACTING'},
+                    //     ]
                     
                         
-                    },
+                    // },
                     value: '',
                     
-                    validation: {},
-                      valid: false,
-                      touched: false
+                    // validation: {},
+                    //   valid: false,
+                    //   touched: false
                 },
                
             progress: {
@@ -97,65 +103,65 @@ export class AddProjects extends Component {
                     }
                 },
             BillingType: {
-                  elementType: 'selectTypeFacturation',
-                  elementConfig: {
+                  //elementType: 'selectTypeFacturation',
+                //   elementConfig: {
                       
-                    options: 
-                    [
+                //     options: 
+                //     [
                         
-                      {value: 'fixe', displayValue: 'Coût fixe'},
-                      {value: 'Par Heures de Projet', displayValue: 'Par Heure de projet'},
-                      {value: 'Par Heures de Tache', displayValue: 'Par Heure de tâche '},
-                    ]
-                  },
+                //       {value: 'fixe', displayValue: 'Coût fixe'},
+                //       {value: 'Par Heures de Projet', displayValue: 'Par Heure de projet'},
+                //       {value: 'Par Heures de Tache', displayValue: 'Par Heure de tâche '},
+                //     ]
+                //   },
                   value: '',
-                  validation: {},
-                  valid: true,
-                  touched: false
+                //   validation: {},
+                //   valid: true,
+                //   touched: false
                    },
                 totalCost: {
-                    elementType: 'inputCoupTotal',
-                    elementConfig: {
-                        type: 'number',
+                    // elementType: 'inputCoupTotal',
+                    // elementConfig: {
+                    //     type: 'number',
                         
-                    },
+                    // },
                     value: '',
-                    validation: {
-                        required: true,
-                        maxLength: 4
-                    },
-                    valid: true,
-                    touched: false
+                    // validation: {
+                    //     required: true,
+                    //     maxLength: 4
+                    // },
+                    // valid: true,
+                    // touched: false
                 } ,
                 status: {
-                    elementType: 'selectStatus',
-                    elementConfig: {
-                      options: 
-                      [
-                        {value: 'En Cours', displayValue: 'En cours'},
-                        {value: 'Non Commencé', displayValue: 'Non Commencé'},
-                        {value: 'En Attente', displayValue: 'En Attente'},
-                        {value: 'Annulé', displayValue: 'Annulé'},
-                        {value: 'Fini', displayValue: 'Achevés'},
-                      ]
-                    },
+                   // elementType: 'selectStatus',
+                    // elementConfig: {
+                    //   options: 
+                    //   [
+                    //     {value: 'En Cours', displayValue: 'En cours'},
+                    //     {value: 'Non Commencé', displayValue: 'Non Commencé'},
+                    //     {value: 'En Attente', displayValue: 'En Attente'},
+                    //     {value: 'Annulé', displayValue: 'Annulé'},
+                    //     {value: 'Fini', displayValue: 'Achevés'},
+                    //   ]
+                    // },
                     value: '',
-                    validation: {},
-                    valid: true,
-                    touched: false
+                    // validation: {},
+                    // valid: true,
+                    // touched: false
                   },
               
                 description: {
-                    elementType: 'textareaDescription',
-                    elementConfig: {
-                        type: 'textarea',
+                    // elementType: 'textareaDescription',
+                    // elementConfig: {
+                    //     type: 'textarea',
                   
-                    },
+                    // },
                     value: '',
-                    validation: {
-                        required: true
-                      },
-                      valid: false
+                //     validation: {
+                //         required: true
+                //       },
+                //       valid: false
                       
     
                  },
@@ -323,8 +329,98 @@ export class AddProjects extends Component {
         let form = (
     
         <Form className=" col-lg-12 mt-5" onSubmit={this.projectDataHandler} >  
-        
-            {
+          <Form.Group controlId="formGridAddress1">
+        <Form.Label>* Nom du projet </Form.Label>
+        <Form.Control size="lg" type="text" placeholder="Nom du projet"  />
+    </Form.Group>
+
+    <Form.Group controlId="formGridAddress2">
+        <Form.Label>* Client</Form.Label>
+        <Form.Control size="lg" as="select" placeholder="Client" defaultValue="Sélectionner">
+          
+                <option> Sélectionner</option>
+                <option>MSBI</option>
+                <option>STAROIL</option>
+                <option>ECM+</option>
+                <option>BRITSH GAS TUNISIA</option>
+                <option>STAR CONTRACTING</option>
+                <option>MARAFIQ YANBU</option>
+        </Form.Control>
+    </Form.Group>
+    <Form.Group>
+      
+      <Form.Label>Emplacement du Projet</Form.Label>
+      <Form.Row>
+          <ContryRegionComponent/>
+           
+      </Form.Row>
+
+    </Form.Group>
+    <Form.Group id="checkProgress">
+        <Form.Check type="checkbox" label="Calculer la progressions à travers les tâches"/>
+    </Form.Group>
+    <Form.Row>
+        <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Type de Facturation</Form.Label>
+            <Form.Control as="select">
+                <option>Coût fixe</option>
+                <option>Par Heure de projet</option>
+                <option>Par Heure de tche</option>
+            </Form.Control>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Statut</Form.Label>
+            <Form.Control as="select" defaultValue="Choose...">
+                <option>En cours</option>
+                <option>Achevés</option>
+                <option>Annulé</option>
+                <option>Non Commencé</option>
+                <option>En Attente</option>
+            </Form.Control>
+        </Form.Group>
+    </Form.Row>
+
+    <Form.Group controlId="formGridAddress2">
+            <Form.Label>Coût total</Form.Label>
+            <InputGroup className="mb-3">
+            <FormControl
+            placeholder="0"
+            aria-label="Amount (to the nearest dollar)"
+            type="number"
+            />
+        <InputGroup.Append>
+        <InputGroup.Text>DT</InputGroup.Text>
+        <InputGroup.Text>0.00</InputGroup.Text>
+        </InputGroup.Append>
+             </InputGroup>
+    </Form.Group>
+
+
+        <Form.Group controlId="formGridAddress2">
+        <Form.Label>Catégorie du projet</Form.Label>
+            <Form.Control size="lg" as="select"  defaultValue="Sélectionner">
+                    <option> Sélectionner</option>
+                    <option>Gaz et Pétrole</option>
+                    <option>Traitement de l'eau</option>
+                    <option>Energie renouvelable</option>
+                    <option>Operation et Maintenance</option>
+                    <option>Industrie</option>
+            </Form.Control>
+
+        </Form.Group>
+
+
+<Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Description du projet : </Form.Label>
+       <TextAreaComponent/>
+</Form.Group>
+    
+<Form.Group id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Envoyé l'e-mail de projet créé" />
+</Form.Group>
+
+
+            {/* {
             formElementsArray.map(formElement => (
                 <Input 
                     key={formElement.id}
@@ -336,17 +432,18 @@ export class AddProjects extends Component {
                     // shouldValidate={formElement.config.validation}
                     touched={formElement.config.touched}
                 />
-            ))}
+                ))
+            } */}
             <hr/>
             
-                <Form.Group >
+                 <Form.Group>
                     <Button variant="success" disabled={this.state.formIsValid} onClick={this.projectDataHandler} className="mr-10"
                         >Ajouter
                     </Button>
                     <Button variant="danger" type="submit" className="" onClick={this.canceledHandler}>
                         Annuler
                     </Button>
-                </Form.Group>
+                 </Form.Group>
            
         </Form >
       
